c70477207a90beed120f73eceac0f7ed4b4933b9
Michal Turcan <michal.turcan.za@gmail.com>
2023-07-03 14:36:49 +0200

    setup static export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ['gravedigger.backbonelabs.io', 'www.erisprotocol.com', 'raw.githubusercontent.com'] }
-}
+  output: 'export',
+  images: {
+    domains: [
+      'gravedigger.backbonelabs.io',
+      'www.erisprotocol.com',
+      'raw.githubusercontent.com',
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
