6b69b81b536dede854062cfa7babd02ab753979d
Joshua <joshua.brigati@gmail.com>
2023-08-03 09:27:10 -0500

    adding url to openGraph data

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ export const metadata = {
     title: 'Alliance Analytics Dashboard',
     description: 'Dashboard for Alliance Analytics',
     siteName: 'Alliance Analytics Dashboard',
+    url: "https://alliance-dashboard.terra.money/",
     images: [
       {
         url: '/opengraph-image.png',
