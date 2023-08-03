57e1322d975abd300dd65d9885bf181b3fb476ba
Joshua <joshua.brigati@gmail.com>
2023-08-03 08:44:39 -0500

    vercel didnt work...

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,6 @@ import '@/styles/globals.css';
 import Nav from '@/components/Nav';
 import { Footer } from '@/components/Footer';
 
-export const metadata = {
-  title: 'Alliance Analytics Dashboard',
-  description: 'by Big labs',
-  openGraph: {
-    type: 'website',
-    title: 'Alliance Analytics Dashboard',
-    description: 'by Big labs',
-    siteName: 'Alliance Analytics Dashboard',
-  },
-}
-
 export default function RootLayout({
   children,
 }: {
